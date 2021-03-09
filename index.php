718b543e1f5f080c754f61c18b3de2f2f1a8fa8f
{доена} <egorodand@gmail.com>
2021-03-09 22:11:26 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="header">
+		<div class="wrapper">
+			<div class="left">
+				
+			</div>
+			<div class="right">
+				
+			</div>
+		</div>
+	</div>
+</body>
+</html>
